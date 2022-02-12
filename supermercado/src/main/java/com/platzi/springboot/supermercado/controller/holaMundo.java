23a3ad2557ec1b0529099beeedd653349b13f754
jhoanmoncada <jhoanmoncada@wposs.com>
2022-02-12 10:28:28 -0500

    java spring avanzado:
     - Se crear estructura del proyecto

---
+++ supermercado/src/main/java/com/platzi/springboot/supermercado/controller/holaMundo.java
@@ -0,0 +1,16 @@
+package com.platzi.springboot.supermercado.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="controller-holaMundo")
+public class holaMundo {
+
+    @GetMapping(path="/saludar")
+    public String saludar(){
+        return "nunca pares de aprender!";
+    }
+
+}
